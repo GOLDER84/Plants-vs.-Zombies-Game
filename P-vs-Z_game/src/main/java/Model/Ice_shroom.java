45d3714dd8a0e211413be2dac6e4c6495821de80
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:31:26 +0330

    Ice_shroom java class created

---
+++ P-vs-Z_game/src/main/java/Model/Ice_shroom.java
@@ -0,0 +1,15 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Ice_shroom extends ExplosivePlants{
+    public Ice_shroom() {
+        super("ice_shroom", 75, 10, new ImageView(new Image("file:src/main/resources/Gifs/ice_shroom.gif")), Power.LOW, 45);
+    }
+
+
+//    public Ice_shroom() {
+//        super("ice_shroom", 75, new ImageView(new Image("file:src/main/resources/Gifs/ice_shroom.gif")), Power.LOW, 45);
+//    }
+}
