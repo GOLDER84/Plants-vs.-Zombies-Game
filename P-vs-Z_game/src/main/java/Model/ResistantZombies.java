11e43247e8238b7f267805fc87ba39d131cbba4f
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:14:42 +0330

    Zombie subsets java class created

---
+++ P-vs-Z_game/src/main/java/Model/ResistantZombies.java
@@ -0,0 +1,11 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ResistantZombies extends Zombie{
+
+    public ResistantZombies(int healthLevel, Power advanceSpeed, Power damagePower, Image animationFrames) {
+        super(healthLevel, advanceSpeed, damagePower, animationFrames);
+    }
+}
---
+++ P-vs-Z_game/src/main/java/Model/SpecialZombies.java
@@ -0,0 +1,46 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class SpecialZombies extends Zombie{
+
+    public SpecialZombies(int healthLevel, Power advanceSpeed, Power damagePower, Image animationFrames) {
+        super(healthLevel, advanceSpeed, damagePower, animationFrames);
+    }
+
+//    //
+//    private String specialAbility;
+//    //اینو هم شک دارم
+//    private Power variableResistance;
+//    private Power variableSpeed;
+
+//        this.specialAbility = specialAbility;
+//        this.variableResistance = variableResistance;
+//        this.variableSpeed = variableSpeed;
+
+
+//    public String getSpecialAbility() {
+//        return specialAbility;
+//    }
+//
+//    public void setSpecialAbility(String specialAbility) {
+//        this.specialAbility = specialAbility;
+//    }
+//
+//    public Power getVariableResistance() {
+//        return variableResistance;
+//    }
+//
+//    public void setVariableResistance(Power variableResistance) {
+//        this.variableResistance = variableResistance;
+//    }
+//
+//    public Power getVariableSpeed() {
+//        return variableSpeed;
+//    }
+//
+//    public void setVariableSpeed(Power variableSpeed) {
+//        this.variableSpeed = variableSpeed;
+//    }
+}
