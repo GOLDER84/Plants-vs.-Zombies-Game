3db575474bfc7caae4090f559a9273612cbc0cfa
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:31:52 +0330

    Normal_Zombie java class created

---
+++ P-vs-Z_game/src/main/java/Model/Normal_Zombie.java
@@ -0,0 +1,10 @@
+package Model;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Normal_Zombie extends BasicZombies{
+    public Normal_Zombie() {
+        super(10, Power.MEDIUM, Power.LOW, new Image("file:src/main/resources/Gifs/normal_zombie.gif"));
+        setEatingAnimation(new Image("file:src/main/resources/Gifs/ZombieAttack.gif"));
+    }
+}
