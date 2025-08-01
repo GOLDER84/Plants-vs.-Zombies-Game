9d45d2d1e19d010f912bf9adaad42f98ac863fef
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:16:09 +0330

    Flag_Zombie java class created

---
+++ P-vs-Z_game/src/main/java/Model/Flag_Zombie.java
@@ -0,0 +1,17 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Flag_Zombie extends SpecialZombies{
+
+
+    public Flag_Zombie() {
+        super(5 , Power.MEDIUM , Power.LOW , new Image("file:src/main/resources/Gifs/flag_zombie.gif"));
+        setEatingAnimation(new Image("file:src/main/resources/Gifs/FlagZombieAttack.gif"));
+    }
+
+    //    public Flag_Zombie(int healthLevel, Power advanceSpeed, Power damagePower, Image zombieView) {
+//        super(healthLevel, advanceSpeed, damagePower, zombieView);
+//    }
+}
