0ea6ade5b243fdc4c5e67fe7ffc2812b06ab5221
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:30:31 +0330

    Cherry_bomb java class created

---
+++ P-vs-Z_game/src/main/java/Model/Cherry_bomb.java
@@ -0,0 +1,14 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Cherry_bomb extends ExplosivePlants{
+    public Cherry_bomb() {
+        super("cherry_bomb", 150, 5, new ImageView(new Image("file:src/main/resources/Gifs/cherry_bomb.gif")), Power.HIGH, 9);
+    }
+
+//    public Cherry_bomb() {
+//        super("cherry_bomb", 150, new ImageView(new Image("file:src/main/resources/Gifs/cherry_bomb.gif")), Power.HIGH, 9);
+//    }
+}
