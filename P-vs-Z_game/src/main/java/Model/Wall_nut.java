290ac96907443442610749cfbacffd4462d3cc32
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:34:20 +0330

    Wall_nut java class created

---
+++ P-vs-Z_game/src/main/java/Model/Wall_nut.java
@@ -0,0 +1,10 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Wall_nut extends DefensePlants{
+    public Wall_nut() {
+        super("wall_nut", 50 , 20, new ImageView(new Image("file:src/main/resources/Gifs/wall_nut.gif")));
+    }
+}
