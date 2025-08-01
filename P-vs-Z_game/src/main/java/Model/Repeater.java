d26c364f095c55014e73645ea9644819d965a678
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:32:41 +0330

    Repeater java class created

---
+++ P-vs-Z_game/src/main/java/Model/Repeater.java
@@ -0,0 +1,14 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Repeater extends WarriorPlants{
+    public Repeater() {
+        super("repeater", 200, 10, new ImageView(new Image("file:src/main/resources/Gifs/repeater.gif")), Power.HIGH, Power.HIGH, BulletType.NORMAL);
+    }
+
+//    public Repeater() {
+//        super("repeater", 200, new ImageView(new Image("file:src/main/resources/Gifs/repeater.gif")), Power.HIGH, Power.HIGH, BulletType.NORMAL);
+//    }
+}
