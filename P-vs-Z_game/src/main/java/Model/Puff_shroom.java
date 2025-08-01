46e011b7cbca1fa8559fcb65adfd379c98f3daee
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-01 22:32:21 +0330

    Puff_shroom java class created

---
+++ P-vs-Z_game/src/main/java/Model/Puff_shroom.java
@@ -0,0 +1,14 @@
+package Model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Puff_shroom extends WarriorPlants{
+    public Puff_shroom() {
+        super("puff_shroom", 0, 10, new ImageView(new Image("file:src/main/resources/Gifs/puff_shroom.gif")),  Power.LOW, Power.HIGH, BulletType.SMOKY);
+    }
+
+//    public Puff_shroom() {
+//        super("puff_shroom", 0, new ImageView(new Image("file:src/main/resources/Gifs/puff_shroom.gif")), Power.LOW, Power.HIGH, BulletType.SMOKY);
+//    }
+}
