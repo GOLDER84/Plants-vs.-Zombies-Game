88ff08c708775aeaca7224aea3d650a8b2dad0e6
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-02 10:31:58 +0330

    HomeController java class created.

---
+++ P-vs-Z_game/src/main/java/org/example/pvsz_game/HomeController.java
@@ -0,0 +1,168 @@
+package org.example.pvsz_game;
+
+import Controller.DataBaseController;
+import Controller.PlayerController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+public class HomeController implements Initializable {
+    private Stage stage;
+    PlayerController playerController = PlayerController.getInstance();
+    DataBaseController dataBaseController = DataBaseController.getInstance();
+
+    @FXML
+    private ImageView ADVButton;
+
+    @FXML
+    private Label playerName;
+
+    @FXML
+    private ImageView MORButton;
+
+    @FXML
+    private ImageView propectice;
+
+    @FXML
+    private ImageView settingsBtn;
+
+    @FXML
+    private Button achievementsBtn;
+
+    @FXML
+    private Button changeBtn;
+
+    @FXML
+    private Button leaderboardsBtn;
+
+    @FXML
+    private Label level;
+
+    @FXML
+    private Label dayOrNight;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //
+//                String path = "src/main/resources/musics/02. Crazy Dave (Intro Theme).mp3";
+//                Media media = new Media(new File(path).toURI().toString());
+//                MediaPlayer mediaPlayer = new MediaPlayer(media);
+//                mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+//                mediaPlayer.setVolume(0.5);
+//                mediaPlayer.play();
+         //
+        String fxmlPath;
+        if (playerController.lastSignedUpPlayer.getCurrentStage() < 4) {
+            fxmlPath = "stepsDay.fxml";
+        } else {
+            fxmlPath = "stepsNight.fxml";
+        }
+        ADVButton.setOnMouseClicked((MouseEvent event) -> {
+            try {
+                this.stage = HelloApplication.primaryStage;
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+                Parent root = fxmlLoader.load();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        playerName.setText(dataBaseController.getUsernameByID(playerController.lastSignedUpPlayer.getId()));
+        level.setText(playerController.lastSignedUpPlayer.getCurrentStage() + "");
+        dayOrNight.setText(playerController.lastSignedUpPlayer.getCurrentStage() < 4 ? "Day" : "Night");
+
+    }
+
+    @FXML
+    void dragDetected(MouseEvent event) {
+        ADVButton.setOpacity(1);
+    }
+
+    @FXML
+    void dragDetected2(MouseEvent event) {
+        ADVButton.setOpacity(0);
+    }
+
+
+    @FXML
+    void MORDetected(MouseEvent event) {
+        MORButton.setOpacity(1);
+    }
+
+    @FXML
+    void MORDetected2(MouseEvent event) {
+        MORButton.setOpacity(0);
+    }
+
+    @FXML
+    void achievementsClicked(MouseEvent event) throws IOException {
+        this.stage = HelloApplication.primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("info.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void changeClicked(MouseEvent event) throws IOException{
+        this.stage = HelloApplication.primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("userManagement.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setX(400);
+        stage.setY(200);
+        stage.show();
+    }
+
+    @FXML
+    void leaderboardsClicked(MouseEvent event) throws IOException{
+        this.stage = HelloApplication.primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("leaderboard.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setX(400);
+        stage.setY(200);
+        stage.show();
+    }
+
+    @FXML
+    void stnClicked(MouseEvent event) {
+        if (propectice.getOpacity() == 0) {
+            propectice.setOpacity(1);
+        } else {
+            propectice.setOpacity(0);
+        }
+    }
+
+    @FXML
+    void settingsBtn(MouseEvent event) throws IOException{
+        this.stage = HelloApplication.primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("settings.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setX(400);
+        stage.setY(200);
+        stage.show();
+    }
+}
