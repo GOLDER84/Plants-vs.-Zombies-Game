accd896cdc842fe75698489ced90381c39f34379
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-04 23:36:00 +0330

    Speed for Flag_Zombie changed.

--- P-vs-Z_game/src/main/java/Model/Flag_Zombie.java
+++ P-vs-Z_game/src/main/java/Model/Flag_Zombie.java
@@ -7,7 +7,7 @@ public class Flag_Zombie extends SpecialZombies{
 
 
     public Flag_Zombie() {
-        super(5 , Power.MEDIUM , Power.LOW , new Image("file:src/main/resources/Gifs/flag_zombie.gif"));
+        super(10 , Power.MEDIUM , Power.LOW , new Image("file:src/main/resources/Gifs/flag_zombie.gif"));
         setEatingAnimation(new Image("file:src/main/resources/Gifs/FlagZombieAttack.gif"));
     }
 
