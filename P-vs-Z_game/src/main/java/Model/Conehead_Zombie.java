6f36ff3a52c06826cb6f788ad6f424e9d40368e0
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-04 23:35:44 +0330

    Speed for Conehead_Zombie changed.

--- P-vs-Z_game/src/main/java/Model/Conehead_Zombie.java
+++ P-vs-Z_game/src/main/java/Model/Conehead_Zombie.java
@@ -5,7 +5,7 @@ import javafx.scene.image.ImageView;
 
 public class Conehead_Zombie extends ResistantZombies{
     public Conehead_Zombie() {
-        super(5, Power.LOW, Power.LOW, new Image("file:src/main/resources/Gifs/conehead_zombie.gif"));
+        super(15, Power.LOW, Power.LOW, new Image("file:src/main/resources/Gifs/conehead_zombie.gif"));
         setEatingAnimation(new Image("file:src/main/resources/Gifs/ConeheadZombieAttack.gif"));
     }
 }
