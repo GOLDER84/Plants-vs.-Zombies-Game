7ae246eb2f8cce716b3cfa873bf6c5e1c3789a16
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-04 13:25:13 +0330

    Commented code in Puff_shroom java class cleaned.

--- P-vs-Z_game/src/main/java/Model/Puff_shroom.java
+++ P-vs-Z_game/src/main/java/Model/Puff_shroom.java
@@ -7,8 +7,4 @@ public class Puff_shroom extends WarriorPlants{
     public Puff_shroom() {
         super("puff_shroom", 0, 10, new ImageView(new Image("file:src/main/resources/Gifs/puff_shroom.gif")),  Power.LOW, Power.HIGH, BulletType.SMOKY);
     }
-
-//    public Puff_shroom() {
-//        super("puff_shroom", 0, new ImageView(new Image("file:src/main/resources/Gifs/puff_shroom.gif")), Power.LOW, Power.HIGH, BulletType.SMOKY);
-//    }
 }
