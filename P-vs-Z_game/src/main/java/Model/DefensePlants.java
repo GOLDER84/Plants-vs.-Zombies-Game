fe025d6c645158fc461d3649d499dca581c8c207
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-06 14:59:33 +0330

    Commented codes in DefensePlants java class cleared.

--- P-vs-Z_game/src/main/java/Model/DefensePlants.java
+++ P-vs-Z_game/src/main/java/Model/DefensePlants.java
@@ -4,9 +4,7 @@ import javafx.scene.image.ImageView;
 
 public class DefensePlants extends Plant {
 
-
     public DefensePlants(String plantName, int plantPrice, int health, ImageView plantView) {
         super(plantName, plantPrice, health, plantView);
     }
-
 }
