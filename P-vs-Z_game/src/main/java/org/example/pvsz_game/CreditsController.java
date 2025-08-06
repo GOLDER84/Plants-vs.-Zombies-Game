d89d8ea84cbb5143eea0f164bb3b7157a62be5c2
Aref Zargar <aref.unlimited.84@gmail.com>
2025-08-06 14:57:59 +0330

    Commented codes in CreditsController java class cleared.

--- P-vs-Z_game/src/main/java/org/example/pvsz_game/CreditsController.java
+++ P-vs-Z_game/src/main/java/org/example/pvsz_game/CreditsController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
